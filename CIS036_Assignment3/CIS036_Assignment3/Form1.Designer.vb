4fd28dae2f2d60f5ce6954391981a9f3531630da
AndrewLam97 <alam1997@gmail.com>
2019-09-18 02:02:39 -0700

    add CIS036_Assignment3

---
+++ CIS036_Assignment3/CIS036_Assignment3/Form1.Designer.vb
@@ -0,0 +1,65 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.picOne = New System.Windows.Forms.PictureBox()
+        Me.btnParkImage = New System.Windows.Forms.Button()
+        CType(Me.picOne, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'picOne
+        '
+        Me.picOne.Image = Global.CIS036_Assignment3.My.Resources.Resources.xB8B_toxUQCJHgBdioyLCMUJ0Zuv_3osLg8x6c1kx8c
+        Me.picOne.Location = New System.Drawing.Point(104, 65)
+        Me.picOne.Name = "picOne"
+        Me.picOne.Size = New System.Drawing.Size(100, 50)
+        Me.picOne.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.picOne.TabIndex = 0
+        Me.picOne.TabStop = False
+        '
+        'btnParkImage
+        '
+        Me.btnParkImage.Location = New System.Drawing.Point(120, 174)
+        Me.btnParkImage.Name = "btnParkImage"
+        Me.btnParkImage.Size = New System.Drawing.Size(75, 23)
+        Me.btnParkImage.TabIndex = 1
+        Me.btnParkImage.Text = "Button1"
+        Me.btnParkImage.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.btnParkImage)
+        Me.Controls.Add(Me.picOne)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.picOne, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents picOne As PictureBox
+    Friend WithEvents btnParkImage As Button
+End Class
---
+++ CIS036_Assignment3/CIS036_Assignment3/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("CIS036_Assignment3")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("CIS036_Assignment3")>
+<Assembly: AssemblyCopyright("Copyright ©  2019")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("ca72993b-bca8-4f9a-a4a7-1c512e9260f8")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
